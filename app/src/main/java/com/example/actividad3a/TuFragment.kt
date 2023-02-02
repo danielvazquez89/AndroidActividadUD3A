0355eda5200c3ff4ebf6063a0c3b08de3e6bcf04
Daniel Vázquez Pérez <danielvazquezperez5@gmail.com>
2023-02-02 10:56:27 +0100

    Foto perfil editable

--- app/src/main/java/com/example/actividad3a/TuFragment.kt
+++ app/src/main/java/com/example/actividad3a/TuFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.actividad3a.databinding.FragmentTuBinding
@@ -39,5 +40,9 @@ class TuFragment : Fragment() {
         mainRecyclerView.layoutManager = GridLayoutManager(context, 1)
 
         mainRecyclerView.adapter = mAdapter
+        binding.irAPerfilLayout.setOnClickListener {
+            val directions = TuFragmentDirections.actionTuFragmentToPerfilFragment()
+            findNavController().navigate(directions)
+        }
     }
 }
