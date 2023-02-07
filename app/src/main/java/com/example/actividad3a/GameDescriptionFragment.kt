d37985ae2df9a28946e7390799c0d67a10d9d3a0
HabieruSenso <javier.guerra@live.u-tad.com>
2023-02-07 09:21:39 +0100

    Javi Creation 6

--- app/src/main/java/com/example/actividad3a/GameDescriptionFragment.kt
+++ app/src/main/java/com/example/actividad3a/GameDescriptionFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -23,12 +24,22 @@ class GameDescriptionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         activity?.findViewById<BottomNavigationView>(R.id.bottom_navigation)?.isVisible = false
+
         val btAddGameToHome = view.findViewById<Button>(R.id.btAddGameToHome)
         btAddGameToHome.setOnClickListener {
             Log.i("Pepa", "ebtra ya")
             val action = GameDescriptionFragmentDirections.actionGameDescriptionFragmentToTuFragment()
             findNavController().navigate(action)
         }
+
+        val tvNameUsertoPerfileUser = view.findViewById<TextView>(R.id.tvSeller)
+        tvNameUsertoPerfileUser.setOnClickListener {
+            Log.i("Pepa", "vas a usuario perfil")
+            val action = GameDescriptionFragmentDirections.actionGameDescriptionFragmentToPerfilUsuarioFragment()
+            findNavController().navigate(action)
+        }
+
     }
 }
