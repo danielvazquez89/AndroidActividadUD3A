39ed84436207f5e6c761dcee2cf8c086c6445b09
Daniel Vázquez Pérez <danielvazquezperez5@gmail.com>
2023-02-08 11:40:48 +0100

    users-api

--- app/src/main/java/com/example/actividad3a/ApiService.kt
+++ app/src/main/java/com/example/actividad3a/ApiService.kt
@@ -2,11 +2,20 @@ package com.example.actividad3a
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("generos")
     fun getGeneros(): Call<GenerosResponse>
+
+    @GET("users")
+    fun getUsers(): Call<UsersResponse>
+
+    @GET("users/{id}")
+    fun getUserById(
+        @Path(value = "id", encoded = false) key: Int
+    ): Call<UsersResponse.UsersResponseItem>
 /*
     @GET("discover/movie")
     fun getMoviesByGenre(
---
+++ app/src/main/java/com/example/actividad3a/UsersResponse.kt
@@ -0,0 +1,23 @@
+package com.example.actividad3a
+
+
+import com.google.gson.annotations.SerializedName
+
+class UsersResponse : ArrayList<UsersResponse.UsersResponseItem>(){
+    data class UsersResponseItem(
+        val apellidos: String,
+        val ciudad: String,
+        @SerializedName("codigo_postal")
+        val codigoPostal: Int,
+        val contrasena: String,
+        val direccion: String,
+        @SerializedName("fecha_nacimiento")
+        val fechaNacimiento: String,
+        @SerializedName("id_usuario")
+        val idUsuario: Int,
+        val mail: String,
+        val nombre: String,
+        @SerializedName("saldo_monedero")
+        val saldoMonedero: Double
+    )
+}
